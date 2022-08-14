17d0e35f1d95626cf2cbc73e8df6f31c7b9def23
Pablo Cesar Fierro <pablofierrovallejos@hotmail.com>
2022-08-14 00:37:12 -0200

    agrega administracion transbank

---
+++ WindowsApp2/bin/administraciontbk.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class administraciontbk
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(administraciontbk))
+        Me.cmdPolling = New System.Windows.Forms.Button()
+        Me.cmdManualTbk = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdPolling
+        '
+        Me.cmdPolling.Location = New System.Drawing.Point(39, 322)
+        Me.cmdPolling.Name = "cmdPolling"
+        Me.cmdPolling.Size = New System.Drawing.Size(141, 26)
+        Me.cmdPolling.TabIndex = 0
+        Me.cmdPolling.Text = "Polling"
+        Me.cmdPolling.UseVisualStyleBackColor = True
+        '
+        'cmdManualTbk
+        '
+        Me.cmdManualTbk.Location = New System.Drawing.Point(39, 287)
+        Me.cmdManualTbk.Name = "cmdManualTbk"
+        Me.cmdManualTbk.Size = New System.Drawing.Size(141, 29)
+        Me.cmdManualTbk.TabIndex = 1
+        Me.cmdManualTbk.Text = "Cambio POSTbk Manual"
+        Me.cmdManualTbk.UseVisualStyleBackColor = True
+        '
+        'administraciontbk
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmdManualTbk)
+        Me.Controls.Add(Me.cmdPolling)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "administraciontbk"
+        Me.Text = "Administración Transbank"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmdPolling As Button
+    Friend WithEvents cmdManualTbk As Button
+End Class
---
+++ WindowsApp2/bin/administraciontbk.vb
@@ -0,0 +1,51 @@
+﻿Imports Transbank.POSIntegrado
+Imports Transbank.Responses.CommonResponses
+Imports Transbank.Responses.IntegradoResponse
+
+Public Class administraciontbk
+    Private Sub cmdPolling_Click(sender As Object, e As EventArgs) Handles cmdPolling.Click
+        Dim msgNOK As String
+        Dim msgOK As String
+        msgNOK = "Problema de conexión con POS"
+        msgOK = "Polling OK"
+
+        Dim pos = POSIntegrado.Instance
+
+        Try
+            Dim booleanPoll = pos.Poll.Result
+            If booleanPoll Then
+                MsgBox(msgOK)
+            Else
+                MsgBox(msgNOK)
+            End If
+        Catch ex As Exception
+            '
+            Debug.Print(ex.StackTrace.ToString)
+            MsgBox(msgNOK)
+        End Try
+
+    End Sub
+
+    Private Sub cmdManualTbk_Click(sender As Object, e As EventArgs) Handles cmdManualTbk.Click
+        Dim msgNOK As String
+        Dim msgOK As String
+        msgNOK = "Cambio Modo Normal Error!"
+        msgOK = "Cambio Modo Normal OK"
+
+        Dim pos = POSIntegrado.Instance
+
+        Try
+            Dim booleanPoll = pos.SetNormalMode
+
+            ' If booleanPoll Then
+            '   MsgBox(msgOK)
+            '    Else
+            '   MsgBox(msgNOK)
+            '   End If
+        Catch ex As Exception
+            '
+            Debug.Print(ex.StackTrace.ToString)
+            MsgBox(msgNOK)
+        End Try
+    End Sub
+End Class
