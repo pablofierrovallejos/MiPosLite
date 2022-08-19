6bbd25df952b578109d62f84cc10f61b32f11fa5
pablofierrovallejos <pablofierrovallejos@hotmail.com>
2022-08-18 21:58:29 -0400

    Actualiza forma para ver resumen de ventas

--- WindowsApp2/bin/ventas.Designer.vb
+++ WindowsApp2/bin/ventas.Designer.vb
@@ -25,31 +25,123 @@ Partial Class ventas
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ventas))
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.Label5 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 30)
+        Me.DataGridView1.Location = New System.Drawing.Point(4, 75)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(972, 433)
+        Me.DataGridView1.Size = New System.Drawing.Size(1048, 433)
         Me.DataGridView1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 469)
+        Me.Button1.Location = New System.Drawing.Point(4, 514)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(105, 30)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Ver ventas"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(455, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ventas"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(198, 531)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Monto Total:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(186, 555)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Total Artículos:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(152, 578)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Total Ventas (boletas):"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(265, 531)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(265, 552)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(265, 573)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(804, 514)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!)
+        Me.Label5.Location = New System.Drawing.Point(539, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(26, 25)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "()"
+        '
         'ventas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(988, 609)
+        Me.ClientSize = New System.Drawing.Size(1068, 685)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -57,9 +149,19 @@ Partial Class ventas
         Me.Text = "Ventas"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents Label5 As Label
 End Class
--- WindowsApp2/bin/ventas.vb
+++ WindowsApp2/bin/ventas.vb
@@ -1,12 +1,31 @@
 ﻿Public Class ventas
+    Dim totalVentas As Integer
+    Dim totalMonto As Integer
+    Dim totalArticulos As Integer
+    Dim dt As Date = Today
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DataGridView1.Rows.Clear()
+        DataGridView1.Columns.Clear()
         IMPORTARCSV(DataGridView1, ";")
     End Sub
 
     ' Sub IMPORTARCSV(ByVal OFD As OpenFileDialog, ByVal TABLA As DataGridView, ByVal DELIMITADOR As String)
     Sub IMPORTARCSV(ByVal TABLA As DataGridView, ByVal DELIMITADOR As String)
+        Dim numeroLinea As Integer = 0
+        Dim secuenciaActual As Integer = 0
+        Dim secuenciaAnterior As Integer
+        totalVentas = 0
+        totalMonto = 0
+        totalArticulos = 0
+
+
+        Label5.Text = dt
+        Dim sdate As String = Replace(dt, "-", "")
+
         Try
-            Dim RUTA As String = "C:\ventasPOS\baseventas\ventasPOS14082022.csv"
+
+            Dim RUTA As String = "C:\ventasPOS\baseventas\ventasPOS" & sdate & ".csv"
 
             ' OFD.InitialDirectory = "C:\TEMP\"
             'OFD.Filter = "CSV FILES (*.CSV)|*.CSV"
@@ -25,14 +44,57 @@
                 Do While OBJREADER.Peek() <> -1
                     TEXTLINE = OBJREADER.ReadLine()
                     SPLITLINE = Split(TEXTLINE, DELIMITADOR)
+                    If numeroLinea > 0 Then
+                        totalMonto = totalMonto + Integer.Parse(SPLITLINE.GetValue(5))
+                        totalArticulos = totalArticulos + Integer.Parse(SPLITLINE.GetValue(4))
+                        secuenciaActual = Integer.Parse(SPLITLINE.GetValue(2))
+                        If (secuenciaActual <> secuenciaAnterior) Then
+                            secuenciaAnterior = secuenciaActual
+                            totalVentas = totalVentas + 1
+                        Else
+                            secuenciaAnterior = secuenciaActual
+                        End If
+                    End If
                     TABLA.ColumnCount = SPLITLINE.Length - 1
                     TABLA.Rows.Add(SPLITLINE)
+                    numeroLinea = numeroLinea + 1
                 Loop
+                OBJREADER.Close()
             Else
                 MsgBox("ARCHIVO INEXISTENTE", MsgBoxStyle.Information, "CSV INEXISTENTE " & RUTA)
             End If
+
+
         Catch EX As Exception
             MsgBox("ERROR DE IMPORTACION: " + EX.ToString)
         End Try
+        TextBox1.Text = totalMonto
+        TextBox2.Text = totalArticulos
+        TextBox3.Text = totalVentas
+    End Sub
+
+    Private Sub ventas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        IMPORTARCSV(DataGridView1, ";")
+    End Sub
+
+    Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs) Handles MonthCalendar1.DateChanged
+        DataGridView1.Rows.Clear()
+        DataGridView1.Columns.Clear()
+
+        dt = MonthCalendar1.SelectionStart.Date
+        IMPORTARCSV(DataGridView1, ";")
+    End Sub
+
+    Private Sub MonthCalendar1_Click(sender As Object, e As EventArgs) Handles MonthCalendar1.Click
+
+
+    End Sub
+
+    Private Sub MonthCalendar1_DoubleClick(sender As Object, e As EventArgs) Handles MonthCalendar1.DoubleClick
+
+    End Sub
+
+    Private Sub MonthCalendar1_MouseClick(sender As Object, e As MouseEventArgs) Handles MonthCalendar1.MouseClick
+
     End Sub
 End Class
