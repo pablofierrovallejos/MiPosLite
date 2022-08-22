cfc2dfb60299ae23e3842008ac500650f9e7c03a
pablofierrovallejos <pablofierrovallejos@hotmail.com>
2022-08-21 21:58:29 -0400

    Implementa funcionalidades administrativas de transbank con async await

--- WindowsApp2/bin/administraciontbk.Designer.vb
+++ WindowsApp2/bin/administraciontbk.Designer.vb
@@ -28,11 +28,14 @@ Partial Class administraciontbk
         Me.cmdCargaLlaves = New System.Windows.Forms.Button()
         Me.cmdDetalleVentas = New System.Windows.Forms.Button()
         Me.cmdUltmaVenta = New System.Windows.Forms.Button()
+        Me.cmdCierre = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.cmdPrintVentasPos = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'cmdPolling
         '
-        Me.cmdPolling.Location = New System.Drawing.Point(39, 259)
+        Me.cmdPolling.Location = New System.Drawing.Point(8, 322)
         Me.cmdPolling.Name = "cmdPolling"
         Me.cmdPolling.Size = New System.Drawing.Size(144, 40)
         Me.cmdPolling.TabIndex = 0
@@ -41,16 +44,16 @@ Partial Class administraciontbk
         '
         'cmdManualTbk
         '
-        Me.cmdManualTbk.Location = New System.Drawing.Point(39, 211)
+        Me.cmdManualTbk.Location = New System.Drawing.Point(8, 271)
         Me.cmdManualTbk.Name = "cmdManualTbk"
         Me.cmdManualTbk.Size = New System.Drawing.Size(144, 40)
         Me.cmdManualTbk.TabIndex = 1
-        Me.cmdManualTbk.Text = "Cambio POSTbk Manual"
+        Me.cmdManualTbk.Text = "Cambio POSTbk Normal (OK)"
         Me.cmdManualTbk.UseVisualStyleBackColor = True
         '
         'cmdCargaLlaves
         '
-        Me.cmdCargaLlaves.Location = New System.Drawing.Point(39, 67)
+        Me.cmdCargaLlaves.Location = New System.Drawing.Point(8, 67)
         Me.cmdCargaLlaves.Name = "cmdCargaLlaves"
         Me.cmdCargaLlaves.Size = New System.Drawing.Size(144, 40)
         Me.cmdCargaLlaves.TabIndex = 2
@@ -59,27 +62,59 @@ Partial Class administraciontbk
         '
         'cmdDetalleVentas
         '
-        Me.cmdDetalleVentas.Location = New System.Drawing.Point(39, 115)
+        Me.cmdDetalleVentas.Location = New System.Drawing.Point(8, 169)
         Me.cmdDetalleVentas.Name = "cmdDetalleVentas"
         Me.cmdDetalleVentas.Size = New System.Drawing.Size(144, 40)
         Me.cmdDetalleVentas.TabIndex = 3
-        Me.cmdDetalleVentas.Text = "Detalle de Ventas"
+        Me.cmdDetalleVentas.Text = "Detalle de Ventas(OK)"
         Me.cmdDetalleVentas.UseVisualStyleBackColor = True
         '
         'cmdUltmaVenta
         '
-        Me.cmdUltmaVenta.Location = New System.Drawing.Point(39, 163)
+        Me.cmdUltmaVenta.Location = New System.Drawing.Point(8, 220)
         Me.cmdUltmaVenta.Name = "cmdUltmaVenta"
         Me.cmdUltmaVenta.Size = New System.Drawing.Size(144, 40)
         Me.cmdUltmaVenta.TabIndex = 4
         Me.cmdUltmaVenta.Text = "Ultima Venta(OK)"
         Me.cmdUltmaVenta.UseVisualStyleBackColor = True
         '
+        'cmdCierre
+        '
+        Me.cmdCierre.Location = New System.Drawing.Point(8, 118)
+        Me.cmdCierre.Name = "cmdCierre"
+        Me.cmdCierre.Size = New System.Drawing.Size(144, 40)
+        Me.cmdCierre.TabIndex = 5
+        Me.cmdCierre.Text = "Cierre"
+        Me.cmdCierre.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(158, 54)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(622, 433)
+        Me.TextBox1.TabIndex = 6
+        '
+        'cmdPrintVentasPos
+        '
+        Me.cmdPrintVentasPos.Location = New System.Drawing.Point(158, 493)
+        Me.cmdPrintVentasPos.Name = "cmdPrintVentasPos"
+        Me.cmdPrintVentasPos.Size = New System.Drawing.Size(144, 40)
+        Me.cmdPrintVentasPos.TabIndex = 7
+        Me.cmdPrintVentasPos.Text = "Imprimir Ventas en POS"
+        Me.cmdPrintVentasPos.UseVisualStyleBackColor = True
+        '
         'administraciontbk
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(790, 583)
+        Me.Controls.Add(Me.cmdPrintVentasPos)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cmdCierre)
         Me.Controls.Add(Me.cmdUltmaVenta)
         Me.Controls.Add(Me.cmdDetalleVentas)
         Me.Controls.Add(Me.cmdCargaLlaves)
@@ -89,6 +124,7 @@ Partial Class administraciontbk
         Me.Name = "administraciontbk"
         Me.Text = "Administración Transbank"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -97,4 +133,7 @@ Partial Class administraciontbk
     Friend WithEvents cmdCargaLlaves As Button
     Friend WithEvents cmdDetalleVentas As Button
     Friend WithEvents cmdUltmaVenta As Button
+    Friend WithEvents cmdCierre As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents cmdPrintVentasPos As Button
 End Class
--- WindowsApp2/bin/administraciontbk.vb
+++ WindowsApp2/bin/administraciontbk.vb
@@ -1,81 +1,189 @@
 ﻿Imports Transbank.POSAutoservicio
+Imports Transbank.POSIntegrado
+Imports Transbank.Exceptions.CommonExceptions
+Imports Transbank.Exceptions.IntegradoExceptions
 Imports Transbank.Responses.CommonResponses
-Imports Transbank.Responses.IntegradoResponse
+Imports Transbank.Responses
 Imports Transbank.Responses.AutoservicioResponse.LastSaleResponse
-
+'Imports Transbank.Responses.AutoservicioResponse
+Imports Transbank.Responses.IntegradoResponses
 
 Public Class administraciontbk
-    Private Sub cmdPolling_Click(sender As Object, e As EventArgs) Handles cmdPolling.Click
-        Dim msgNOK As String
-        Dim msgOK As String
-        msgNOK = "Problema de conexión con POS"
-        msgOK = "Polling OK"
 
-        Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
-        POSAutoservicio.Instance.OpenPort(portName)
-
-        'Dim Task_resp = POSAutoservicio.Instance.Poll()
-
-        'Task<bool> pollResult = Task.Run(async () => { return await POSIntegrado.Instance.Poll(); });
-        TestPoll()
-
+    Private Sub cmdPolling_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdPolling.Click
         Try
-            Dim booleanPoll = True ' Task_resp.Result.ToString
-            If booleanPoll Then
-                MsgBox(msgOK, MsgBoxStyle.Information, "ESTADO POLLING")
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSAutoservicio.Instance.OpenPort(portName)
+
+            Dim pollResult As Task(Of Boolean) = Task.Run(Async Function() Await POSAutoservicio.Instance.Poll())
+            Dim sout = pollResult.Wait(30000)
+
+            If sout Then  'If pollResult.Result Then
+                'MessageBox.Show("POS conectado correctamente a PC Autoatención.", "Polling the POS")
+                MsgBox("POS conectado correctamente a PC Autoatención.", vbInformation, "miAutoPOS")
             Else
-                MsgBox(msgOK, MsgBoxStyle.Information, "ESTADO POLLING")
+                MsgBox("Error en Comunicación con POS,no se pudo realizar Poll ", vbInformation, "miAutoPOS")
             End If
-        Catch ex As Exception
-            Debug.Print(ex.StackTrace.ToString)
-            MsgBox(msgNOK)
+
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
         End Try
-        POSAutoservicio.Instance.ClosePort()
+    End Sub
+
+    Private Sub cmdManualTbk_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdManualTbk.Click
+        Try
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSIntegrado.Instance.OpenPort(portName)
+
+            Dim dialogResult As DialogResult = MessageBox.Show("La configuración en Modo Normal desconectará el POS" & vbLf & " Está Seguro?", "Set Modo Normal", MessageBoxButtons.YesNo)
+
+            If dialogResult = DialogResult.Yes Then
+                Dim result As Task(Of Boolean) = Task.Run(Async Function() Await POSIntegrado.Instance.SetNormalMode())
+                Dim sout = result.Wait(30000)
+
+                If sout Then  'If result.Result Then
+                    MsgBox("POS configurado en Modo Normal", vbInformation, "miAutoPOS")
+                Else
+                    MsgBox("Error en Comunicación, no se pudo cambiar POS a modo Normal", vbInformation, "miAutoPOS")
+                End If
+            End If
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+    End Sub
+
+    ' Private Sub cmdUltmaVenta_Click(sender As Object, e As EventArgs) Handles cmdUltmaVenta.Click
+    ' Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+    '    POSAutoservicio.Instance.OpenPort(portName)
+    ' Dim lastStaleResponse = POSAutoservicio.Instance.LastSale()
+    '     Debug.Print(lastStaleResponse.Result.ToString)
+    ' End Sub
+
+    Private Sub cmdUltmaVenta_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdUltmaVenta.Click
+        Try
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSAutoservicio.Instance.OpenPort(portName)
+
+            Dim response As Task(Of LastSaleResponse) = Task.Run(Async Function() Await POSIntegrado.Instance.LastSale())
+            Dim sout = response.Wait(30000)
+
+            If sout Then
+                If response.Result.Success Then
+                    MessageBox.Show(response.Result.ToString(), "Última venta obtenida satisfacoriamente.")
+                End If
+            Else
+                MessageBox.Show("Falló la conexión con POS.")
+            End If
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
     End Sub
 
 
-    Async Function TestPoll() As Task(Of Boolean)
-        Dim getEstados As Task(Of Boolean) = POSAutoservicio.Instance.Poll()
-        Dim resp As Boolean = Await getEstados
-        Return resp
-    End Function
 
-    Private Sub cmdManualTbk_Click(sender As Object, e As EventArgs) Handles cmdManualTbk.Click
-        Dim msgNOK As String
-        Dim msgOK As String
-        msgNOK = "Cambio Modo Normal Error!"
-        msgOK = "Cambio Modo Normal OK"
-
-        Dim pos = POSAutoservicio.Instance
-
+    Private Sub cmdDetalleVentas_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdDetalleVentas.Click
         Try
-            Dim booleanPoll = pos.Poll
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSIntegrado.Instance.OpenPort(portName)
 
-            ' If booleanPoll Then
-            '   MsgBox(msgOK)
-            '    Else
-            '   MsgBox(msgNOK)
-            '   End If
-        Catch ex As Exception
-            '
-            Debug.Print(ex.StackTrace.ToString)
-            MsgBox(msgNOK)
+
+            Dim details As Task(Of List(Of DetailResponse)) = Task.Run(Async Function() Await POSIntegrado.Instance.Details(False))
+            Dim resp = details.Wait(30000)
+            Dim stmp As String = ""
+
+            If resp Then
+                For Each detail As DetailResponse In details.Result
+                    'stmp = detail.CardType
+                    stmp = "Tipo de Tarjeta : " & detail.CardType
+
+                    stmp = stmp & "     Total : " & detail.Amount
+
+                    stmp = stmp & "     Ultimos 4Digitos : " & detail.Last4Digits
+
+                    stmp = stmp & "     Fecha : " & detail.RealDate & vbNewLine
+
+
+                    TextBox1.Text = TextBox1.Text & stmp
+                Next
+            Else
+                MsgBox("Error en Comunicación, no se pudo obtener detalle de Ventas", vbInformation, "miAutoPOS")
+            End If
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+    End Sub
+
+    Private Sub cmdPrintVentasPos_Click(sender As Object, e As EventArgs) Handles cmdPrintVentasPos.Click
+        Try
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSIntegrado.Instance.OpenPort(portName)
+
+            TextBox1.Text = ""
+            Dim details As Task(Of List(Of DetailResponse)) = Task.Run(Async Function() Await POSIntegrado.Instance.Details(True))
+            Dim resp = details.Wait(30000)
+            Dim stmp As String = ""
+
+            If resp Then
+                For Each detail As DetailResponse In details.Result
+
+                    stmp = "Tipo de Tarjeta : " & detail.CardType
+
+                    stmp = stmp & "     Total : " & detail.Amount
+
+                    stmp = stmp & "     Ultimos 4Digitos : " & detail.Last4Digits
+
+                    stmp = stmp & "     Fecha : " & detail.RealDate & vbNewLine
+
+                    TextBox1.Text = TextBox1.Text & stmp
+                Next
+            Else
+                MsgBox("Error en Comunicación, no se pudo obtener detalle de Ventas", vbInformation, "miAutoPOS")
+            End If
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+    End Sub
+
+    Private Sub cmdCierre_Click(ByVal sender As Object, ByVal e As EventArgs) Handles cmdCierre.Click
+        Try
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSIntegrado.Instance.OpenPort(portName)
+
+            Dim response As Task(Of CloseResponse) = Task.Run(Async Function() Await POSIntegrado.Instance.Close())
+            Dim resp = response.Wait(30000)
+
+            If resp Then
+                If response.Result.Success Then
+                    MessageBox.Show(response.Result.ToString(), "Operación de Cierre.")
+                End If
+            Else
+                MsgBox("Error en Comunicación, no se pudo realizar Cierre", vbInformation, "miAutoPOS")
+            End If
+            MessageBox.Show(response.Result.ToString(), "Operación de Cierre.")
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
         End Try
     End Sub
 
     Private Sub cmdCargaLlaves_Click(sender As Object, e As EventArgs) Handles cmdCargaLlaves.Click
 
+        Try
+            Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+            POSIntegrado.Instance.OpenPort(portName)
 
+            Dim response As Task(Of LoadKeysResponse) = Task.Run(Async Function() Await POSIntegrado.Instance.LoadKeys())
+            Dim resp = response.Wait(30000)
 
+            If resp Then
+                If response.Result.Success Then
+                    MessageBox.Show(response.Result.ToString(), "Keys Cargada Satisfactoriamente.")
+                End If
+            Else
+                MsgBox("Error en Comunicación, no se pudo cargar Keys", vbInformation, "miAutoPOS")
+            End If
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
 
     End Sub
-
-    Private Sub cmdUltmaVenta_Click(sender As Object, e As EventArgs) Handles cmdUltmaVenta.Click
-        Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
-        POSAutoservicio.Instance.OpenPort(portName)
-        Dim lastStaleResponse = POSAutoservicio.Instance.LastSale()
-        Debug.Print(lastStaleResponse.Result.ToString)
-    End Sub
-
-
 End Class
