e8185b2d458edacf5a93eca7031dad03b8c8138a
Pablo Cesar Fierro <pablofierrovallejos@hotmail.com>
2022-10-30 21:18:50 -0200

    corrige bugs control

---
+++ WindowsApp2/bin/AnularVenta.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AnularVenta
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AnularVenta))
+        Me.lblAccion = New System.Windows.Forms.Label()
+        Me.txbNroOpe = New System.Windows.Forms.TextBox()
+        Me.btnAnular = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblEstado = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblAccion
+        '
+        Me.lblAccion.AutoSize = True
+        Me.lblAccion.Location = New System.Drawing.Point(25, 77)
+        Me.lblAccion.Name = "lblAccion"
+        Me.lblAccion.Size = New System.Drawing.Size(87, 13)
+        Me.lblAccion.TabIndex = 0
+        Me.lblAccion.Text = "Núm. Operación:"
+        '
+        'txbNroOpe
+        '
+        Me.txbNroOpe.Location = New System.Drawing.Point(117, 75)
+        Me.txbNroOpe.Name = "txbNroOpe"
+        Me.txbNroOpe.Size = New System.Drawing.Size(205, 20)
+        Me.txbNroOpe.TabIndex = 1
+        '
+        'btnAnular
+        '
+        Me.btnAnular.Location = New System.Drawing.Point(28, 134)
+        Me.btnAnular.Name = "btnAnular"
+        Me.btnAnular.Size = New System.Drawing.Size(144, 40)
+        Me.btnAnular.TabIndex = 2
+        Me.btnAnular.Text = "Anular"
+        Me.btnAnular.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(178, 134)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(144, 40)
+        Me.btnCancelar.TabIndex = 3
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblEstado)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(353, 200)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(99, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(139, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Anulación Venta Transbank"
+        '
+        'lblEstado
+        '
+        Me.lblEstado.AutoSize = True
+        Me.lblEstado.Location = New System.Drawing.Point(22, 187)
+        Me.lblEstado.Name = "lblEstado"
+        Me.lblEstado.Size = New System.Drawing.Size(10, 13)
+        Me.lblEstado.TabIndex = 1
+        Me.lblEstado.Text = "."
+        '
+        'AnularVenta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(358, 207)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAnular)
+        Me.Controls.Add(Me.txbNroOpe)
+        Me.Controls.Add(Me.lblAccion)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "AnularVenta"
+        Me.Text = "Anular Venta Transbank"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAccion As Label
+    Friend WithEvents txbNroOpe As TextBox
+    Friend WithEvents btnAnular As Button
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblEstado As Label
+End Class
---
+++ WindowsApp2/bin/AnularVenta.vb
@@ -0,0 +1,60 @@
+﻿Imports Transbank.Exceptions.CommonExceptions
+Imports Transbank.POSIntegrado
+
+Public Class AnularVenta
+    Dim posTimeout As String = Integer.Parse(readConfig("POS_TIMEOUT_MS"))
+    Private Sub btnAnular_Click(sender As Object, e As EventArgs) Handles btnAnular.Click
+        Dim op As Integer = Convert.ToInt32(txbNroOpe.Text)
+
+        Dim portName As String = readConfig("COM_TRANSBANK")    'Viene de modulo bass
+
+
+        Try
+            POSIntegrado.Instance.OpenPort(portName)
+
+            Dim response As Task(Of Transbank.Responses.CommonResponses.RefundResponse) = Task.Run(Async Function() Await POSIntegrado.Instance.Refund(op))
+
+            lblEstado.Text = If(response.Status = 1, "WaitingForActivation", "")
+
+            Dim resp = response.Wait(posTimeout)
+
+            If resp Then
+                ' MessageBox.Show(response.ToString(), "Refund Success.")
+                MessageBox.Show("Respuesta OK", "Anulación")
+                Me.Close()
+            Else
+                If response.Status = TaskStatus.WaitingToRun Then
+                    MsgBox("Tiempo superado de espera.", vbInformation, "miAutoPOS")
+                ElseIf response.Status = TaskStatus.WaitingForActivation Then
+                    MsgBox("Aún no computado.", vbInformation, "miAutoPOS")
+                Else
+                    MsgBox("Error en Comunicación o Venta no Existe, no se pudo realizar Anulación", vbInformation, "miAutoPOS")
+                End If
+            End If
+            POSIntegrado.Instance.ClosePort()
+            lblEstado.Text = ""
+        Catch a As TransbankException
+            MessageBox.Show(a.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txbNroOpe_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txbNroOpe.KeyPress
+
+        '97 - 122 = Ascii codes for simple letters
+        '65 - 90  = Ascii codes for capital letters
+        '48 - 57  = Ascii codes for numbers
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+
+    End Sub
+End Class
+
+
